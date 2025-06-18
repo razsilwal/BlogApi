36ff928ba1572b277ceced3cc0a37b9d15be3557
razsilwal <sabalsilwal51@gmail.com>
2025-06-18 22:03:24 +0545

    now its working properly

--- blog/models.py
+++ blog/models.py
@@ -4,17 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    CATEGORY_CHOICES = [
-        ('tech', 'Technology'),
-        ('life', 'Lifestyle'),
-        ('news', 'News'),
-        ('other', 'Other'),
-    ]
+
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     upload = models.FileField(upload_to='uploads/', blank=True, null=True)
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='other')
+    category = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
